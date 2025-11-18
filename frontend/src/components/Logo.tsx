4630f3f6b672e279bf2a263ffa8431951be11e4a
jdiaz_eryx <jdiaz@eryx.co>
2025-11-18 12:34:01 +0000

    Changes logo

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import cypherPassLogo from '../assets/cypherpass-logo.png'
 import './Logo.css'
 
 function Logo() {
@@ -16,55 +17,7 @@ function Logo() {
 
   return (
     <div className={`logo ${isVisible ? 'visible' : 'hidden'}`}>
-      <svg 
-        className="logo-icon" 
-        width="40" 
-        height="40" 
-        viewBox="0 0 40 40" 
-        fill="none" 
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {/* Ticket/Circuit board shape */}
-        <rect 
-          x="4" 
-          y="8" 
-          width="32" 
-          height="24" 
-          rx="4" 
-          fill="#220844"
-        />
-        {/* Rounded cutout on left side */}
-        <path 
-          d="M4 16C4 14.8954 4.89543 14 6 14H8C9.10457 14 10 14.8954 10 16V20C10 21.1046 9.10457 22 8 22H6C4.89543 22 4 21.1046 4 20V16Z" 
-          fill="transparent"
-        />
-        {/* Inner blue elements */}
-        <rect 
-          x="8" 
-          y="14" 
-          width="8" 
-          height="8" 
-          rx="2" 
-          fill="#5A74FC"
-        />
-        <rect 
-          x="18" 
-          y="16" 
-          width="4" 
-          height="4" 
-          rx="1" 
-          fill="#5A74FC"
-        />
-        <rect 
-          x="24" 
-          y="16" 
-          width="4" 
-          height="4" 
-          rx="1" 
-          fill="#5A74FC"
-        />
-      </svg>
-      <span className="logo-text">cypherpass</span>
+      <img className="logo-image" src={cypherPassLogo} alt="CypherPass logo" />
     </div>
   )
 }
